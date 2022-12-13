32e622b90eacfb8992ea271651dc3acc8d5df62d
YudhistiraKusuma202310067 <202310067@ibik.ac.id>
2022-12-13 23:20:20 +0700

    Update Auth

--- src/main/java/com/duasatuwo/api/user/UserRepo.java
+++ src/main/java/com/duasatuwo/api/user/UserRepo.java
@@ -1,7 +1,11 @@
 package com.duasatuwo.api.user;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepo extends CrudRepository<User, Integer>{
     
+    @Query(value = "SELECT a.* FROM user a WHERE a.email = :email AND a.password = :password", nativeQuery = true)
+    public Iterable<User> findUserAuth(@Param("email") String email, @Param("password") String password);
 }
--- src/main/java/com/duasatuwo/api/user/UserService.java
+++ src/main/java/com/duasatuwo/api/user/UserService.java
@@ -23,4 +23,8 @@ public class UserService {
     public Iterable<User> findAll(){
         return userRepo.findAll();
     }
+
+    public Iterable<User> findAuth(String email, String password){
+        return userRepo.findUserAuth(email, password);
+      }
 }
