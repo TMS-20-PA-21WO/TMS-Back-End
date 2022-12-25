bf5d977651eeb536b79441f5b61a6c41b1d16316
YudhistiraKusuma202310067 <202310067@ibik.ac.id>
2022-12-25 13:43:01 +0700

    progress token

--- src/main/java/com/duasatuwo/api/user/UserController.java
+++ src/main/java/com/duasatuwo/api/user/UserController.java
@@ -2,6 +2,9 @@ package com.duasatuwo.api.user;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,8 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.duasatuwo.api.dto.AuthentificationKey;
 import com.duasatuwo.api.dto.GetEmail;
 import com.duasatuwo.api.dto.ResponseData;
+import com.duasatuwo.api.helper.MyHelpers;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import javax.validation.Valid;
+
+import com.duasatuwo.api.user.User;
 
 @RestController
 @RequestMapping("/api/user")
@@ -97,25 +107,64 @@ public class UserController {
         }
     }
 
+    // @PostMapping("/auth")
+    // public ResponseEntity<ResponseData<User>> getStudentAuth(@RequestBody AuthentificationKey authentificationKey) {
+    //     ResponseData<User> responseData = new ResponseData<>();
+
+    //     try {
+    //         Iterable<User> values = userService.findAuth(authentificationKey.getEmail(),
+    //                 authentificationKey.getPassword());
+    //         responseData.setResult(true);
+    //         responseData.getMessage();
+    //         responseData.setData(values);
+    //         return ResponseEntity.ok(responseData);
+
+    //     } catch (Exception e) {
+    //         List<String> message = new ArrayList<>();
+    //         message.add(e.getMessage());
+    //         responseData.setMessage(message);
+    //         responseData.setData(null);
+    //         responseData.setResult(false);
+    //         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+    //     }
+    // }
+
     @PostMapping("/auth")
-    public ResponseEntity<ResponseData<User>> getStudentAuth(@RequestBody AuthentificationKey authentificationKey) {
-        ResponseData<User> responseData = new ResponseData<>();
-
+    public ResponseEntity<Map<String,String>> getStudentAuth(@RequestBody String payload) {
+        JsonObject jobj = new Gson().fromJson(payload, JsonObject.class);
+        String email = jobj.get("email").getAsString();
+        String password = jobj.get("password").getAsString();
+    
         try {
-            Iterable<User> values = userService.findAuth(authentificationKey.getEmail(),
-                    authentificationKey.getPassword());
-            responseData.setResult(true);
-            responseData.getMessage();
-            responseData.setData(values);
-            return ResponseEntity.ok(responseData);
-
+            Iterable<User> values = userService.findAuth(email, password);
+    
+            Algorithm algorithm = Algorithm.HMAC256(new MyHelpers().PRIVATE_KEY);
+            String access_token = JWT.create()
+                .withSubject(values.toString())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 + 60 * 1000)) //expired 1 minutes
+                .withIssuer("auth0")
+                .sign(algorithm);
+    
+            String refresh_token = JWT.create()
+                .withSubject(values.toString())
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 + 60 * 1000)) //expired 3 minutes
+                .withIssuer("auth0")
+                .sign(algorithm);
+            
+            Map<String,String> results = new HashMap<>();
+            results.put("access_token",access_token);
+            results.put("refresh_token", refresh_token);
+    
+            return ResponseEntity.ok(results);
+    
         } catch (Exception e) {
             List<String> message = new ArrayList<>();
             message.add(e.getMessage());
-            responseData.setMessage(message);
-            responseData.setData(null);
-            responseData.setResult(false);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+            Map<String,String> response = new HashMap<>();
+            response.put("result", "false");
+            response.put("data", "");
+            response.put("message", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
     }
 
