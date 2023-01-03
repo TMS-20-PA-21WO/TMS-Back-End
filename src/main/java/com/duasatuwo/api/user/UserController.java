1b82a815c9dc45df6f8bdcc6c19c47eb81223a70
fajaralf <202310072@student.ibik.ac.id>
2023-01-03 19:32:31 +0700

    finish

--- src/main/java/com/duasatuwo/api/user/UserController.java
+++ src/main/java/com/duasatuwo/api/user/UserController.java
@@ -108,59 +108,60 @@ public class UserController {
     }
 
     // @PostMapping("/auth")
-    // public ResponseEntity<ResponseData<User>> getStudentAuth(@RequestBody AuthentificationKey authentificationKey) {
-    //     ResponseData<User> responseData = new ResponseData<>();
+    // public ResponseEntity<ResponseData<User>> getStudentAuth(@RequestBody
+    // AuthentificationKey authentificationKey) {
+    // ResponseData<User> responseData = new ResponseData<>();
 
-    //     try {
-    //         Iterable<User> values = userService.findAuth(authentificationKey.getEmail(),
-    //                 authentificationKey.getPassword());
-    //         responseData.setResult(true);
-    //         responseData.getMessage();
-    //         responseData.setData(values);
-    //         return ResponseEntity.ok(responseData);
+    // try {
+    // Iterable<User> values = userService.findAuth(authentificationKey.getEmail(),
+    // authentificationKey.getPassword());
+    // responseData.setResult(true);
+    // responseData.getMessage();
+    // responseData.setData(values);
+    // return ResponseEntity.ok(responseData);
 
-    //     } catch (Exception e) {
-    //         List<String> message = new ArrayList<>();
-    //         message.add(e.getMessage());
-    //         responseData.setMessage(message);
-    //         responseData.setData(null);
-    //         responseData.setResult(false);
-    //         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
-    //     }
+    // } catch (Exception e) {
+    // List<String> message = new ArrayList<>();
+    // message.add(e.getMessage());
+    // responseData.setMessage(message);
+    // responseData.setData(null);
+    // responseData.setResult(false);
+    // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseData);
+    // }
     // }
 
     @PostMapping("/auth")
-    public ResponseEntity<Map<String,String>> getStudentAuth(@RequestBody String payload) {
+    public ResponseEntity<Map<String, String>> getStudentAuth(@RequestBody String payload) {
         JsonObject jobj = new Gson().fromJson(payload, JsonObject.class);
         String email = jobj.get("email").getAsString();
         String password = jobj.get("password").getAsString();
-    
+
         try {
             Iterable<User> values = userService.findAuth(email, password);
-    
+
             Algorithm algorithm = Algorithm.HMAC256(new MyHelpers().PRIVATE_KEY);
             String access_token = JWT.create()
-                .withSubject(values.toString())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 10 + 60 * 1000)) //expired 1 minutes
-                .withIssuer("auth0")
-                .sign(algorithm);
-    
+                    .withSubject(values.toString())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 10 + 120 * 1000)) // expired 1 minutes
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+
             String refresh_token = JWT.create()
-                .withSubject(values.toString())
-                .withExpiresAt(new Date(System.currentTimeMillis() + 500 + 60 * 1000)) //expired 3 minutes
-                .withIssuer("auth0")
-                .sign(algorithm);
-            
-            Map<String,String> results = new HashMap<>();
-            results.put("access_token",access_token);
+                    .withSubject(values.toString())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 30 + 60 * 1000)) // expired 3 minutes
+                    .withIssuer("auth0")
+                    .sign(algorithm);
+
+            Map<String, String> results = new HashMap<>();
+            results.put("access_token", access_token);
             results.put("refresh_token", refresh_token);
-    
+
             return ResponseEntity.ok(results);
-    
+
         } catch (Exception e) {
             List<String> message = new ArrayList<>();
             message.add(e.getMessage());
-            Map<String,String> response = new HashMap<>();
+            Map<String, String> response = new HashMap<>();
             response.put("result", "false");
             response.put("data", "");
             response.put("message", e.getMessage());
